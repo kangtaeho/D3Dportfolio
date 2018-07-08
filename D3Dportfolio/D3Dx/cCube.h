c43f517903270e83da50045a098c2cd7b95c6043
kangkangtaeho <whitevines@naver.com>
2018-07-09 02:55:05 +0900

    ㅇㅇ

---
+++ D3Dportfolio/D3Dx/cCube.h
@@ -0,0 +1,30 @@
+#pragma once
+
+struct ST_MESH
+{
+	ST_PNT_VERTEX v;
+	int index;
+};
+
+class cCube
+{
+public:
+	cCube();
+	~cCube();
+
+private:
+	std::vector<ST_PNT_VERTEX>	m_vecVertex;
+	std::vector<ST_PNT_VERTEX>	m_vecIndex;
+
+	D3DXVECTOR3					m_vPosition;
+	SYNTHESIZE(D3DXMATRIX, m_matWorld, MatWorld);
+
+	ID3DXMesh*					m_pMesh;
+
+	float						m_fRotY;
+public:
+	void Setup(D3DXVECTOR3 size, LPCWSTR texName);
+	void Update();
+	void Render();
+};
+
