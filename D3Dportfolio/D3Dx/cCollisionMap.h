8d9de47786acb679e0da296a4031c13d6f970f1d
ChocoDarling <35556229+ChocoDarling@users.noreply.github.com>
2018-07-10 16:13:10 +0900

    바닥맵 랜더 추가
    바닥맵 랜더 추가

--- D3Dportfolio/D3Dx/cCollisionMap.h
+++ D3Dportfolio/D3Dx/cCollisionMap.h
@@ -10,14 +10,9 @@ public:
 	cCollisionMap();
 	~cCollisionMap();
 
-	void LoadSurface(OUT std::vector<D3DXVECTOR3>& vecSurface,
-		IN const char* szFolder, IN const char* szFile,
-		IN D3DXMATRIX* pMat = NULL);
-	void LoadCollisionSurface(OUT std::vector<stCollisionMap>& VecCollisionFace, IN const char * szFolder, IN const char * szFile, IN D3DXMATRIX* pMat);
-
-	void Render();
-
 	std::vector<D3DXVECTOR3>* getMap() { return &m_pVecSurface; }				// 표면의 버텍스벡터 주소값
 	std::vector<stCollisionMap>* getCollisionMap() { return &m_pVecCollisionFace; }
+
+	void Render();
 };
 
