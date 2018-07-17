5ea61a6da8cd613a3bfee8a2b8c53b1212d7878f
kangkangtaeho <whitevines@naver.com>
2018-07-17 23:54:03 +0900

    커밋다시합니다

--- D3Dportfolio/D3Dx/cSelectScene.cpp
+++ D3Dportfolio/D3Dx/cSelectScene.cpp
@@ -13,9 +13,30 @@ cSelectScene::~cSelectScene()
 
 HRESULT cSelectScene::Setup()
 {
-	m_pMainSelectWin= g_pTextureManager->addTexture("selecteWin", "./select/selectWin.png", NULL, NULL);
+	g_pTextureManager->addTexture("selecteWin", "./select/selectWin.png", NULL, NULL);
+	g_pTextureManager->addTexture("selectPlayerWin01", "./select/selectPlayerWin01.png", NULL, NULL);
+	g_pTextureManager->addTexture("selectPlayerWin02", "./select/selectPlayerWin02.png", NULL, NULL);
+	g_pTextureManager->addTexture("selectRect", "./select/selectRect.png", NULL, NULL);
+	g_pTextureManager->addTexture("cIcon01", "./select/cIcon01.png", NULL, NULL);
+	g_pTextureManager->addTexture("cIcon02", "./select/cIcon02.png", NULL, NULL);
+	g_pTextureManager->addTexture("cIcon03", "./select/cIcon03.png", NULL, NULL);
+	g_pTextureManager->addTexture("cIcon04", "./select/cIcon04.png", NULL, NULL);
+	g_pTextureManager->addTexture("cIcon05", "./select/cIcon05.png", NULL, NULL);
+	g_pTextureManager->addTexture("cIcon06", "./select/cIcon06.png", NULL, NULL);
+	g_pTextureManager->addTexture("cIcon07", "./select/cIcon07.png", NULL, NULL);
+	g_pTextureManager->addTexture("cIcon08", "./select/cIcon08.png", NULL, NULL);
+	g_pTextureManager->addTexture("cIcon09", "./select/cIcon09.png", NULL, NULL);
+	g_pTextureManager->addTexture("cIcon10", "./select/cIcon10.png", NULL, NULL);
+
+	m_pMainSelectWin = g_pTextureManager->findTexture("selectWin");
 	m_pMainSelectWin->setPosition(D3DXVECTOR3(0, 0, 0));
 	m_pMainSelectWin->setScale(D3DXVECTOR3(0, 0, 0));
+
+	m_pPlayerSelect = g_pTextureManager->findTexture("selectPlayerWin01");
+	m_pMainSelectWin->setPosition(D3DXVECTOR3(22, 67, 0));
+	m_pMainSelectWin->setScale(D3DXVECTOR3(0, 0, 0));
+
+	
 
 	return S_OK;
 }
@@ -27,10 +48,24 @@ void cSelectScene::Update()
 
 void cSelectScene::Render()
 {
-	m_pMainSelectWin->Render();
+	if (m_pMainSelectWin)
+		m_pMainSelectWin->Render();
+
+	if (m_pPlayerSelect)
+		m_pPlayerSelect->Render();
+
+	if (m_pPlayerChamp)
+		m_pPlayerChamp->Render();
+
+	if (m_pEnemyChamp)
+		m_pEnemyChamp->Render();
+
+	if (m_pSelect)
+		m_pSelect->Render();
 }
 
 void cSelectScene::Release()
 {
+	if (m_pMainSelectWin)
 	m_pMainSelectWin->release();
 }
--- D3Dportfolio/D3Dx/cSelectScene.h
+++ D3Dportfolio/D3Dx/cSelectScene.h
@@ -1,14 +1,19 @@
 #pragma once
 #include "cGameNode.h"
 
-
 class Bitmap;
 
 class cSelectScene :
 	public cGameNode
 {
 private:
-	Bitmap* m_pMainSelectWin;
+	Bitmap* m_pMainSelectWin;	 // 큰 창
+	Bitmap* m_pPlayerSelect;	 // 플레이어 창
+	Bitmap* m_pPlayerChamp;		 // 플레이어 챔프 창
+	Bitmap* m_pEnemyChamp;		 // 적 챔프 창
+	Bitmap* m_pSelect;			 // 선택 태두리 창
+
+	RECT rc_Select[10];			 // 챔피언 클릭 판정 렉트
 
 public:
 	cSelectScene();
