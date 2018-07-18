13253a8dbd6a3cd5efa67f44a6d90c49930abc3f
성위현 <songwehyun@hotmail.com>
2018-07-18 13:14:50 +0900

    a

--- D3Dportfolio/D3Dx/cTextureManager.h
+++ D3Dportfolio/D3Dx/cTextureManager.h
@@ -13,9 +13,16 @@ private:
 	typedef std::map<std::string, Bitmap*>::iterator mapTextureIter;
 
 	std::map<std::string, LPDIRECT3DTEXTURE9> m_mapTexture;
+<<<<<<< HEAD
 	std::map<std::string, Bitmap> m_maptexture;
 	std::map<std::string, Bitmap>::iterator  m_mapiter;
 	Bitmap* m_items;
+=======
+
+	Bitmap* aaa;
+	Bitmap icon;
+
+>>>>>>> cd7687dcc206c17de3137e344bdcf1b71cea9aa7
 private:
 	mapTextureList m_textureList;
 public:
