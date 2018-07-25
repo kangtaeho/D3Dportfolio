a15412665d541e08e9854e06467f885bf8b357cf
kangkangtaeho <whitevines@naver.com>
2018-07-25 16:15:38 +0900

    dd

--- D3Dportfolio/D3Dx/cEnemy.cpp
+++ D3Dportfolio/D3Dx/cEnemy.cpp
@@ -17,7 +17,8 @@ void cEnemy::Setup(const char * name)
 	m_fRadius = 50;
 	m_fHP = 300;
 	m_vPosition = m_AStar.PushDestination(m_vPosition, m_fRadius); //만약에 충돌을 받으면 밀어낸다
-	// 테스트용입니다
+	m_vPosition = g_pCollisionManager->SetHeight(m_vPosition);
+																   // 테스트용입니다
 
 	cCharacter::Setup(name);
 }
