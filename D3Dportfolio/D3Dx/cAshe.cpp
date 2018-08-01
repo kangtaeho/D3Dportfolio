0a8e76354f6e3f1059dc7b3adaf935c448a1e71d
kangkangtaeho <whitevines@naver.com>
2018-08-01 22:26:16 +0900

    병철아ㅇㅇ

--- D3Dportfolio/D3Dx/cAshe.cpp
+++ D3Dportfolio/D3Dx/cAshe.cpp
@@ -2,6 +2,7 @@
 #include "cAshe.h"
 #include "cAStar.h"
 #include "cRangeSkill.h"
+#include <ctime>
 
 cAshe::cAshe()
 {
@@ -17,7 +18,7 @@ cAshe::~cAshe()
 void cAshe::Setup(const char * name)
 {
 	m_fRange = 400;
-	m_fSpeed = 7.0f;
+	m_fSpeed = 5.0f;
 	m_fMAXHP = 300;
 	m_fMAXMP = 250;
 	m_fHP = 300;
@@ -28,8 +29,15 @@ void cAshe::Setup(const char * name)
 
 	cCharacter::Setup(name);
 
+	m_sMovePos.pos[0]= D3DXVECTOR3(1280, 0, 735);
+	m_sMovePos.pos[1]= D3DXVECTOR3(450, 0, -150);
+	m_sMovePos.index = 0;
+	m_sMovePos.isChange = false;
+
+	m_vStartPos = D3DXVECTOR3(3445, 0, 2700);
 	m_vPosition = D3DXVECTOR3(3445, 0, 2700);
-	m_vNextPosition = D3DXVECTOR3(0, 0, 0);
+	m_vNextPosition = m_sMovePos.pos[0];
+
 }
 
 void cAshe::Release()
@@ -48,12 +56,47 @@ void cAshe::Update()
 	}
 	else
 	{
+		m_sMovePos.isChange = true;
 		setAnimation();
 	}
+
+	ChangePos();
 
 }
 
 void cAshe::Render()
 {
 	cCharacter::Render();
+}
+
+void cAshe::ChangePos()
+{
+	if (m_sMovePos.isChange)
+	{
+		if (m_sMovePos.index == 0)
+		{
+			m_sMovePos.index++;
+			m_sMovePos.isChange = false;
+			m_vNextPosition = m_sMovePos.pos[m_sMovePos.index];
+		}
+		else 
+		{
+			srand(GetTickCount());
+			int rndNum1 = 150 + rand() % 100;
+			int rndNum2 = 150 + rand() % 50;
+
+			int dir1 = 1;
+			int dir2 = 1;
+			if ((rndNum1 % 2) == 0) dir1 *= -1;
+			if ((rndNum2 % 2) == 0) dir2 *= -1;
+
+			m_sMovePos.pos[0] = D3DXVECTOR3(m_vPosition.x + (dir1 *rndNum1), 0, m_vPosition.z + (dir2 *rndNum2));
+			m_sMovePos.pos[1] = m_vPosition;
+			m_sMovePos.index = 0;
+			m_sMovePos.isChange = false;
+
+			m_vNextPosition = m_sMovePos.pos[0];
+		}
+
+	}
 }
--- D3Dportfolio/D3Dx/cAshe.h
+++ D3Dportfolio/D3Dx/cAshe.h
@@ -10,15 +10,24 @@ enum AI_TYPE
 
 class cRangeSkill;
 
+struct AI_POS
+{
+	D3DXVECTOR3 pos[2];
+	int index;
+	bool isChange;
+};
+
 class cAshe : public cCharacter
 {
 private:
 
 	cRangeSkill* m_pAttack;
-	D3DXVECTOR3	 m_vCurrentPos;
 	
 	std::vector<cCharacter*>* m_vecAllEnemy;
-	std::vector<D3DXVECTOR3> m_vecPosAI;
+	std::vector<AI_POS> m_vecPosAI;
+
+	AI_POS m_sMovePos;
+	D3DXVECTOR3 m_vStartPos;
 
 public:
 	cAshe();
@@ -28,6 +37,8 @@ public:
 	virtual void Release();
 	virtual void Update();
 	virtual void Render();
+
+	void ChangePos();
 
 };
 
